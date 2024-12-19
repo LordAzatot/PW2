7738150d6e33498a9d4e852241aa6f9746c1b68f
LordAzatot <mskorotych.fitm23@kubg.edu.ua>
2024-12-20 01:01:55 +0200

    Add files via upload

---
+++ LW№2-КОРОТИЧ_МАКСИМ/Task1/Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Rectangle
+    {
+        private double side1, side2;
+
+        // Конструктор
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+
+        // Метод для обчислення площі
+        public double CalculateArea() => side1 * side2;
+
+        // Метод для обчислення периметра
+        public double CalculatePerimeter() => 2 * (side1 + side2);
+
+        // Властивість для площі
+        public double Area => CalculateArea();
+
+        // Властивість для периметра
+        public double Perimeter => CalculatePerimeter();
+    }
+
+    class Program1
+    {
+        static void Main()
+        {
+            Console.Write("Введіть довжину першої сторони прямокутника: ");
+            double side1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Введіть довжину другої сторони прямокутника: ");
+            double side2 = double.Parse(Console.ReadLine());
+
+            var rectangle = new Rectangle(side1, side2);
+
+            Console.WriteLine($"Площа прямокутника: {rectangle.Area:F2}");
+            Console.WriteLine($"Периметр прямокутника: {rectangle.Perimeter:F2}");
+        }
+    }
+}
---
+++ LW№2-КОРОТИЧ_МАКСИМ/Task2/Task2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Point
+    {
+        public double X { get; }
+        public double Y { get; }
+        public string Name { get; }
+
+        // Конструктор
+        public Point(double x, double y, string name)
+        {
+            X = x;
+            Y = y;
+            Name = name;
+        }
+    }
+
+    class Figure
+    {
+        private Point[] points;
+
+        // Конструктор для 3-5 точок
+        public Figure(params Point[] points)
+        {
+            if (points.Length < 3 || points.Length > 5)
+            {
+                throw new ArgumentException("Багатокутник повинен мати від 3 до 5 точок.");
+            }
+            this.points = points;
+        }
+
+        // Метод для розрахунку довжини сторони
+        public double GetSideLength(Point A, Point B)
+        {
+            return Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
+        }
+
+        // Метод для розрахунку периметра
+        public double CalculatePerimeter()
+        {
+            double perimeter = 0;
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                Point current = points[i];
+                Point next = points[(i + 1) % points.Length]; // Замикання багатокутника
+                perimeter += GetSideLength(current, next);
+            }
+
+            return perimeter;
+        }
+
+        // Властивість для отримання назви
+        public string Name => points.Length switch
+        {
+            3 => "Трикутник",
+            4 => "Чотирикутник",
+            5 => "П'ятикутник",
+            _ => "Невідомий багатокутник"
+        };
+    }
+
+    class Program2
+    {
+        static void Main()
+        {
+            // Створюємо точки
+            var A = new Point(0, 0, "A");
+            var B = new Point(0, 3, "B");
+            var C = new Point(4, 0, "C");
+
+            // Створюємо трикутник
+            var triangle = new Figure(A, B, C);
+
+            Console.WriteLine($"Назва багатокутника: {triangle.Name}");
+            Console.WriteLine($"Периметр багатокутника: {triangle.CalculatePerimeter():F2}");
+
+            // Додатково можна створити чотирикутник чи п'ятикутник
+        }
+    }
+}
